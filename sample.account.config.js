6377329878622fe7609c1ff4339d90a7b66c4a26
kevin-wad <kevin.osseni@groupe-quintesens.fr>
2019-12-18 10:43:48 +0100

    Rename config files to sample.config

---
+++ sample.account.config.js
@@ -0,0 +1,15 @@
+var accounts = {
+
+  minting: {
+    address: '0xf8E93935c104AFf83F2165a9B110Db757632bEf8',
+    privateKey: '00000'
+  },
+
+  payment: {
+    address: '0xf8E93935c104AFf83F2165a9B110Db757632bEf8',
+    privateKey: '00000'
+  }
+
+}
+
+exports.accounts = accounts
