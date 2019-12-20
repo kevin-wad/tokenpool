e31ab87ee8e6327c8e2d378026ef73a0af454127
kevin-wad <kevin.osseni@groupe-quintesens.fr>
2019-12-20 14:28:25 +0100

    Try update webpack to fix issue

--- webpack.config.js
+++ webpack.config.js
@@ -106,14 +106,6 @@ module.exports = {
     plugins: [
         new CopyPlugin([
           { from: 'app/assets', to: 'assets' }
-        ]),
-        new webpack.DefinePlugin({
-            'process.env': {
-              NODE_ENV: '"production"'
-            }
-          }),
-          new ExtractTextPlugin({
-            filename: 'app/assets/main.css'
-         })
+        ])
       ]
 };
